8e4247d64503f81cdbb374e33748a12553b174d4
akacprzak <akacprzak@unity-t.pl>
2018-12-12 10:29:07 +0100

    Sekcja 22

--- src/app/about/reactive-form/reactive-form.component.ts
+++ src/app/about/reactive-form/reactive-form.component.ts
@@ -1,41 +1,81 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators} from '@angular/forms';
+
 
 @Component({
-  selector: 'app-template-driven-form',
-  templateUrl: './template-driven-form.component.html',
-  styleUrls: ['./template-driven-form.component.css']
+    selector: 'app-reactive-form',
+    templateUrl: './reactive-form.component.html',
+    styleUrls: ['./reactive-form.component.css']
 })
-export class TemplateDrivenFormComponent implements OnInit {
+export class ReactiveFormComponent implements OnInit {
 
-@ViewChild('conctactForm')
-conctactForm: NgForm;
+    contactForm: FormGroup;
 
-  message = new TemplateMessage();
-  courses = ['Angular', 'JavaFx', 'Java8', 'OrmLite'];
+    message = new ReactiveMessage();
 
-  constructor() {}
+    courses = ['Angular', 'JavaFx', 'Java8', 'OrmLite'];
 
-  ngOnInit() {}
+    ngOnInit() {
+        this.contactForm = new FormGroup({
+            topic: new FormControl(null, Validators.required),
+            message: new FormControl(null, [Validators.required, Validators.minLength(10)]),
+            name: new FormControl(null, Validators.required),
+            email: new FormControl(null, [Validators.required, Validators.email]),
+            course: new FormControl(this.courses[3]),
+            bot: new FormControl(null),
+            questions: new FormArray([new FormControl(null), new FormControl(null)], this.questionsValidator)
+        });
+        // this.contactForm.valueChanges.subscribe(value => {
+        //     console.log(value);
+        // });
+        this.contactForm.get('topic').valueChanges.subscribe(value => {
+            console.log(value);
+        });
+        this.contactForm.get('email').statusChanges.subscribe(value => {
+            console.log(value);
+        });
+    }
 
-  onSubmit() {
-    console.log(this.message);
-    this.reset();
-  }
+    addQuestions() {
+        const arr = <FormArray>this.contactForm.get('questions');
+        arr.push(new FormControl(null));
+    }
 
-  reset() {
-    this.message = new TemplateMessage();
-    this.conctactForm.resetForm(this.message);
-  }
+    questionsValidator(control: AbstractControl): ValidationErrors {
+        const arr = <[string]>control.value;
+        if (arr.includes('angularjs')) {
+            return {forbbidenCourse: true};
+        }
+    }
 
+    onSubmit() {
+        console.log(this.contactForm);
+        this.message.topic = this.contactForm.get('topic').value;
+        this.message.name = this.contactForm.value.name;
+        this.message.message = this.contactForm.value.message;
+        this.message.email = this.contactForm.value.email;
+        this.message.course = this.contactForm.value.course;
+        this.message.bot = this.contactForm.value.bot;
+        this.onReset();
+    }
+
+    onReset() {
+        this.contactForm.reset({
+            // this.contactForm.patchValue({
+            topic: 'Wow!',
+            // course: this.courses[3]
+        });
+    }
 }
-class TemplateMessage {
-  constructor(
-    public topic?: string,
-    public message?: string,
-    public name?: string,
-    public email?: string,
-    public course = 'Angular',
-    public bot?: boolean
-  ) {}
+
+class ReactiveMessage {
+    constructor(
+        public topic?: string,
+        public message?: string,
+        public name?: string,
+        public email?: string,
+        public course = 'Angular',
+        public bot?: boolean
+    ) {
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CoursesComponent } from './courses/courses.component';
@@ -11,26 +11,22 @@ import { DataBaseService } from './data-base.service';
 import { CourseDetailComponent } from './courses/course-detail/course-detail.component';
 import { CoursesListComponent } from './courses/courses-list/courses-list.component';
 import { TemplateDrivenFormComponent } from './about/template-driven-form/template-driven-form.component';
-
+import { ReactiveFormComponent } from './about/reactive-form/reactive-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CoursesComponent,
-    AboutComponent,
-    PageNotFoundComponent,
-    CourseDetailComponent,
-    CoursesListComponent,
-    TemplateDrivenFormComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [DataBaseService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        CoursesComponent,
+        AboutComponent,
+        PageNotFoundComponent,
+        CourseDetailComponent,
+        CoursesListComponent,
+        TemplateDrivenFormComponent,
+        ReactiveFormComponent
+    ],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+    providers: [DataBaseService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
